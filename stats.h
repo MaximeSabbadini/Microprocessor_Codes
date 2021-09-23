244b6ca8248123131c609f4da13ecc481c6dfd55
MaximeSabbadini <maxime.sabbadini7@gmail.com>
2021-09-23 09:43:29 -0400

    Adding header file

---
+++ stats.h
@@ -0,0 +1,15 @@
+#ifndef __STATS_H__
+#define __STATS_H__
+
+
+void print_statistics(int median, int mean, int maximum, int minimum);
+void print_array(unsigned char *arr, char size);
+float find_median(unsigned char test[]);
+float find_mean(unsigned char test[]);
+float find_maximum(unsigned char test[]);
+float find_minimum(unsigned char test[]);
+void sort_array(usnigned char *sort, char size);
+
+
+
+#endif /* __STATS_H__**/
